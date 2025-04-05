9640432364ea9cc228c1f83513ecd7ab272d6640
Israel Albino Galvan <israel.galvan@live.com>
2025-04-05 02:23:31 -0300

    Test add mapper options tests

---
+++ MiniMapr.Tests/Core/MapperOptionsTests.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+
+namespace MiniMapr.Tests.Core;
+
+public class MapperOptionsTests
+{
+    [Fact]
+    public void AddCustomTransformation_ShouldStoreTransformation()
+    {
+        // Arrange
+        var options = new MapperOptions();
+
+        // Action
+        options.AddCustomTransformation<int, string>("Age", age => $"Idade: {age}");
+
+        // Assert
+        options.CustomTransformations.Should().ContainKey("Age");
+
+        var result = options.CustomTransformations["Age"](25);
+        result.Should().Be($"Idade: {25}");
+    }
+}
